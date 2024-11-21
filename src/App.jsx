1144dbbc9f3345abf978f84f0a76754e579d7014
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-11-21 15:51:22 +0530

    2024.11.21

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Greet from "./components/1-Exercise/Greet";
+import Footer from "./components/2-Exercise/Footer";
+import Header from "./components/2-Exercise/Header";
+import Main from "./components/2-Exercise/Main";
+import WelcomeMessage from "./components/3-Exercise/WelcomeMessage";
+import JSXRuls from "./components/4-Exercise/JSXRuls";
+
+const App = () => {
+  return (
+    <div>
+      {/* <Greet/> */}
+
+      {/* <Header />
+      <Main />
+      <Footer /> */}
+
+      {/* <WelcomeMessage /> */}
+
+      <JSXRuls />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/4-Exercise/JSXRuls.jsx
@@ -0,0 +1,15 @@
+const JSXRuls = () => {
+  return (
+    <div>
+      <h1>JSX Ruls</h1>
+      <p>JSX must return a single parent element</p>
+      <p>JSX element must be properly closed</p>
+      <p>
+        JSX attributes are written using camelCase (e.g.. className instead of
+        class)
+      </p>
+    </div>
+  );
+};
+
+export default JSXRuls;
